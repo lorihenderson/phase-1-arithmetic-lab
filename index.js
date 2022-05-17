df35b6200106d7623c2ec90e1c38b9c776199531
Lori Henderson <lorim95@gmail.com>
2022-05-17 11:10:42 -0400

    commit tree 8d21aa703f2793e32f495de42dfc976900197169

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+
+// multiply
+let num1 = 31;
+let num2 = 2;
+let multiply = num1  * num2;
+
+// random
+let random = Math.ceil(Math.random());
+
+// mod
+let num3 = 9
+let num4 = 5
+let mod = num3 % num4;
+
+// max
+let max = Math.max(1, 3, 5, 20);
